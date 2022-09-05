fb7d97dd51f8a3e8004ff4f03bc8146865c56bd9
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-09-05 21:10:20 +0530

    🚅 Implement '/login'

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,5 +1,6 @@
 import Button from "./button";
 import Icon from "./icon";
 import Logo from "./logo";
+import Input from "./input";
 
-export { Icon, Button, Logo };
+export { Icon, Button, Logo, Input };
