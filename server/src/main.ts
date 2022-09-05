676a7f205ed8c5d8447dfb8f0236ce5fefbb1287
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-09-05 20:27:28 +0530

    🔑  Enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,11 +4,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   await app.listen(8080);
 }
 
